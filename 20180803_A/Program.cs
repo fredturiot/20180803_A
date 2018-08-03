cc46d56878f905ed690a800ff6f11839991a07af
jsurville <julien.surville@gmail.com>
2018-08-03 10:00:22 +0200

    TP animal

--- 20180803_A/Program.cs
+++ 20180803_A/Program.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _20180803_A
+namespace _20180803_A_TP
 {
     class Program
     {
         static void Main(string[] args)
-        {
-        }
+        { }
     }
+    //classes
+    public abstract class Animal
+    {
+        public void Marcher()
+        { }
+    }
+
+    public abstract class Mammifere : Animal
+    {}
+
+    public class Oiseau : Animal
+    {
+        public bool PeutVoler { get; set; }
+    }
+
+    public class Chien : Mammifere
+    {
+        public void Aboyer()
+        { }
+    }
+
+    public class Chat : Mammifere
+    {
+        public void Miauler()
+        { }
+    }
+
+    public class Poule : Oiseau
+    { }
 }
